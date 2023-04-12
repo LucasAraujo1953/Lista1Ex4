96d0e741924213929c66132bcfa635f6cc3e61d7
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-04-12 20:40:24 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double b;
+            double h;
+            double a;
+
+            // Entrada de Dados
+            Console.Write("Informe a base do Triângulo: ");
+            b=double.Parse(Console.ReadLine());
+            Console.Write("Informe a altura do Triângulo: ");
+            h=double.Parse(Console.ReadLine());
+            // Operação
+            a = b * h / 2;
+
+            Console.WriteLine("O valor da base do Triângulo é {0}, e sua altura é {1}, com isso, sua área é igual a {2}", b, h, a);
+
+        }
+    }
+}
